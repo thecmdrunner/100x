fb0d110f609001b33a59cea64e10564d46f210d3
thecmdrunner <thecmdrunner@proton.me>
2023-08-21 16:16:02 +0530

    emoji search and replace

--- 1-chat-app/public/script.js
+++ 1-chat-app/public/script.js
@@ -1,19 +1,44 @@
 const socket = io();
 
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const messages = document.getElementById('messages');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
 
-form.addEventListener('submit', (e) => {
+const emojis = {
+    react: "⚛️",
+    woah: "😮",
+    hey: "👋",
+    lol: "😂",
+    like: "❤️",
+    congratulations: "🎉",
+};
+
+const replaceWordsWithEmojis = (message) => {
+    const words = message.split(" ");
+    const newWords = words.map((word) => {
+        // Removing punctuation from the word before checking for emoji replacement
+        const cleanedWord = word.replace(/[.,!?]/g, "");
+
+        if (emojis?.[cleanedWord]) {
+            return emojis[cleanedWord] + (/[.,!?]/.test(word) ? word[word.length - 1] : "");
+            // Reattach punctuation if it was removed
+        }
+        return word;
+    });
+    return newWords.join(" ");
+};
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
+    const message = input?.value;
+    if (message) {
+        socket.emit("chat message", replaceWordsWithEmojis(message));
+        input.value = "";
     }
 });
 
-socket.on('chat message', (msg) => {
-    const li = document.createElement('li');
+socket.on("chat message", (msg) => {
+    const li = document.createElement("li");
     li.textContent = msg;
     messages.appendChild(li);
 });
