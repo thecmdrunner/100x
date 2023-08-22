df2ce932d2e340fe5ce01c5c97cc23a19f611396
thecmdrunner <thecmdrunner@proton.me>
2023-08-22 14:51:38 +0530

    case insensitive searches, slash commands

--- 1-chat-app/public/script.js
+++ 1-chat-app/public/script.js
@@ -18,10 +18,14 @@ const replaceWordsWithEmojis = (message) => {
     const newWords = words.map((word) => {
         // Removing punctuation from the word before checking for emoji replacement
         const cleanedWord = word.replace(/[.,!?]/g, "");
+        const caseInsensitiveWord = cleanedWord.toLowerCase();
 
-        if (emojis?.[cleanedWord]) {
-            return emojis[cleanedWord] + (/[.,!?]/.test(word) ? word[word.length - 1] : "");
-            // Reattach punctuation if it was removed
+        if (emojis?.[caseInsensitiveWord]) {
+            return (
+                // replace word with emoji, and reattach punctuation if it was removed
+                emojis[caseInsensitiveWord] +
+                (/[.,!?]/.test(word) ? word[word.length - 1] : "")
+            );
         }
         return word;
     });
@@ -31,14 +35,63 @@ const replaceWordsWithEmojis = (message) => {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const message = input?.value;
-    if (message) {
-        socket.emit("chat message", replaceWordsWithEmojis(message));
-        input.value = "";
+    input.value = "";
+
+    // Handle empty message
+    if (!message) {
+        return alert("Please enter a message.");
     }
+
+    // Handle slash commands
+    if (message in slashCommands) {
+        return slashCommands[message].execute();
+    }
+
+    // Handle regular message sent to server
+    socket.emit("chat message", replaceWordsWithEmojis(message));
 });
 
 socket.on("chat message", (msg) => {
-    const li = document.createElement("li");
-    li.textContent = msg;
-    messages.appendChild(li);
+    const listItem = document.createElement("li");
+    listItem.textContent = msg;
+    messages.appendChild(listItem);
 });
+
+const slashCommands = {
+    "/clear": {
+        description: "Clear all messages",
+        execute: () => {
+            while (messages?.firstChild) {
+                messages.removeChild(messages.firstChild);
+            }
+        },
+    },
+    "/help": {
+        description: "List all slash commands",
+        execute: () => {
+            const helpMsg = `Available commands:
+${Object.keys(slashCommands).map(
+                (cmd) => `${cmd}: ${slashCommands[cmd].description}\n`
+            ).join("")}
+            `;
+
+            return alert(helpMsg);
+        },
+    },
+    "/random": {
+        description: "Generate a random number",
+        execute: () => {
+            const randomNumber = Math.floor(Math.random() * 100000);
+            const listItem = document.createElement("li");
+            const boldTag = document.createElement("b");
+            const italicTag = document.createElement("i");
+            italicTag.textContent = `🧑‍🏫 Your random number is ${randomNumber} (only you can view this message)`;
+
+            boldTag.appendChild(italicTag);
+            listItem.appendChild(boldTag);
+            messages.appendChild(listItem);
+        },
+    },
+};
+
+
